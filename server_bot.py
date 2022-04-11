66a326cd4ea3a4903d83befc8b09e7bc72b5f2c9
wall-e-yao <48679464+wall-e-yao@users.noreply.github.com>
2022-04-11 12:10:12 +0800

    Update server_bot.py

--- server_bot.py
+++ server_bot.py
@@ -23,7 +23,7 @@ class INotifier(metaclass=ABCMeta):
         """
 
 import os        
-USERs = eval(os.environ['USERS'])
+USERs = eval(os.environ['USERs'])
 TIMEOUT_SECOND = 25
 
 class ServerJiangNotifier(INotifier):
